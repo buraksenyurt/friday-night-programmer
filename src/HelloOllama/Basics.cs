c0b584b2ac39725c3462ff22c884ae4e5b51a2d3
buraksenyurt <burakselimsenyurt@gmail.com>
2025-02-13 17:42:00 +0300

    Refactored

---
+++ src/HelloOllama/Basics.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.AI;
+
+namespace HelloOllama;
+
+class Basics
+{
+    /*
+         Aşağıdaki sonsuz döngü kısmında istemci nesne birden çok kez çağırılmakta. Bu nedenle ilgili nesneyi 
+         Dependency Injection Container'a alıp oradan tedarik ederek kullandırmak çok daha mantıklı.
+
+         Sonsuz döngü aslında bir chatbot ile konuşma efektini vermek için.
+
+         Sorulan sorular (prompts) bir ChatMessage nesnesi haline getirilerek kullanılmakta. 
+         Aslında taraflar için tek bir nesne modeli var. Bunlardan hangisinin kullanıcıya ait olduğu hangisinin chatbot'tan beklenen 
+         bir mesaj olduğu genellikle ChatRole struct nesnesi üzerinden belirleniyor. (User ve Assistant kullanımlarına dikkat edelim) 
+    */
+    public static async     /*
+         Aşağıdaki sonsuz döngü kısmında istemci nesne birden çok kez çağırılmakta. Bu nedenle ilgili nesneyi 
+         Dependency Injection Container'a alıp oradan tedarik ederek kullandırmak çok daha mantıklı.
+
+         Sonsuz döngü aslında bir chatbot ile konuşma efektini vermek için.
+
+         Sorulan sorular (prompts) bir ChatMessage nesnesi haline getirilerek kullanılmakta. 
+         Aslında taraflar için tek bir nesne modeli var. Bunlardan hangisinin kullanıcıya ait olduğu hangisinin chatbot'tan beklenen 
+         bir mesaj olduğu genellikle ChatRole struct nesnesi üzerinden belirleniyor. (User ve Assistant kullanımlarına dikkat edelim) 
+    */
+    Task
+Run(IChatClient chatClient)
+    {
+        Console.WriteLine("Asistant mode has been started...");
+
+        var chatHistory = new List<ChatMessage>();
+
+        while (true)
+        {
+            Console.WriteLine("> Waiting for command...");
+            var userPrompt = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(userPrompt)) continue;
+            if (userPrompt.Equals("exit", StringComparison.CurrentCultureIgnoreCase)) break;
+            if (userPrompt.Equals("clear", StringComparison.CurrentCultureIgnoreCase))
+            {
+                chatHistory.Clear();
+                Console.WriteLine("Clearing history...");
+                Console.Clear();
+                continue;
+            }
+            chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+            var chatResponse = string.Empty;
+            await foreach (var item in chatClient.CompleteStreamingAsync(chatHistory))
+            {
+                Console.Write(item.Text);
+                chatResponse += item.Text;
+            }
+            chatHistory.Add(new ChatMessage(ChatRole.Assistant, chatResponse));
+            Console.WriteLine();
+        }
+    }
+}
--- src/HelloOllama/Program.cs
+++ src/HelloOllama/Program.cs
@@ -2,9 +2,9 @@
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Diagnostics;
 
 var builder = Host.CreateApplicationBuilder();
+
 /*
     Ollama servisi ile konuşacak bir client nesnesi söz konusu.
     Bu nesne localhost:11434 portundan çalışan servise gidip onun deepseek-r1 ile çalışmasını istiyor. 
@@ -13,63 +13,6 @@ builder.Services.AddChatClient(new OllamaChatClient(new Uri("http://localhost:11
 var app = builder.Build();
 var chatClient = app.Services.GetRequiredService<IChatClient>();
 
-#region Sample 02 - Code Quality Check
+// await Basics.Run(chatClient);
 
-var codeFiles = Directory.GetFiles("C:\\samples", "*.cs").ToArray();
-Console.WriteLine($"Looking for {codeFiles.Length} code files");
-foreach (var codeFile in codeFiles)
-{
-    var time = Stopwatch.StartNew();
-    Console.WriteLine($"Analysing {codeFile}. Time {DateTime.Now.ToLongTimeString()}");
-    var prompt=Prompts.GetLevel200(codeFile);
-    var chatCompletion = await chatClient.CompleteAsync(prompt);
-    Console.WriteLine(chatCompletion.Message.Text);
-    Console.WriteLine(Environment.NewLine);
-    Console.WriteLine($"Total time of analysis {time.Elapsed.TotalSeconds}");
-}
-
-
-#endregion
-
-#region Sample 01 - Classic Chatbot
-
-/*
-    Aşağıdaki sonsuz döngü kısmında istemci nesne birden çok kez çağırılmakta. Bu nedenle ilgili nesneyi 
-    Dependency Injection Container'a alıp oradan tedarik ederek kullandırmak çok daha mantıklı.
-
-    Sonsuz döngü aslında bir chatbot ile konuşma efektini vermek için.
-
-    Sorulan sorular (prompts) bir ChatMessage nesnesi haline getirilerek kullanılmakta. 
-    Aslında taraflar için tek bir nesne modeli var. Bunlardan hangisinin kullanıcıya ait olduğu hangisinin chatbot'tan beklenen 
-    bir mesaj olduğu genellikle ChatRole struct nesnesi üzerinden belirleniyor. (User ve Assistant kullanımlarına dikkat edelim) 
-*/
-//Console.WriteLine("Asistant mode has been started...");
-
-//var chatHistory = new List<ChatMessage>();
-
-//while (true)
-//{
-//    Console.WriteLine("> Waiting for command...");
-//    var userPrompt = Console.ReadLine();
-//    if (string.IsNullOrWhiteSpace(userPrompt)) continue;
-//    if (userPrompt.Equals("exit", StringComparison.CurrentCultureIgnoreCase)) break;
-//    if (userPrompt.Equals("clear", StringComparison.CurrentCultureIgnoreCase))
-//    {
-//        chatHistory.Clear();
-//        Console.WriteLine("Clearing history...");
-//        Console.Clear();
-//        continue;
-//    }
-//    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
-
-//    var chatResponse = string.Empty;
-//    await foreach (var item in chatClient.CompleteStreamingAsync(chatHistory))
-//    {
-//        Console.Write(item.Text);
-//        chatResponse += item.Text;
-//    }
-//    chatHistory.Add(new ChatMessage(ChatRole.Assistant, chatResponse));
-//    Console.WriteLine();
-//}
-
-#endregion
+await TechDebtHunter.CheckCodeQuality(chatClient);
