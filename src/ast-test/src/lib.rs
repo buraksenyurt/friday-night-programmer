de0a3cc0dddec83ef7abfb6228e9c7b7caf11790
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2025-02-17 09:44:13 +0000

    Tried another example

--- src/ast-test/src/lib.rs
+++ src/ast-test/src/lib.rs
@@ -29,14 +29,77 @@ fn test_find_classes() {
 
     let tree = parse(code);
     let root_node = tree.root_node();
-    let mut found_class = false;
+    let mut class_names = Vec::new();
 
     for node in root_node.children(&mut root_node.walk()) {
         if node.kind() == "class_declaration" {
-            found_class = true;
-            assert!(node.child_by_field_name("name").is_some());
+            if let Some(name) = node.child_by_field_name("name") {
+                class_names.push(code[name.byte_range()].to_string());
+            }
         }
     }
 
-    assert!(found_class, "No class found in AST!");
+    assert_eq!(
+        class_names,
+        vec!["GameEngine".to_string(), "Customer".to_string()]
+    );
+}
+
+//TODO@buraksenyurt Extend with return type and method parameters
+#[test]
+fn test_find_methods() {
+    let code = r#"
+        public class Einstein {
+            public double Add(double x, double y) {
+                return x + y;
+            }
+
+            public double Mul(double x, double y) {
+                return x * y;
+            }
+
+            public void ClearTemp() {
+                Console.WriteLine("Clearing temp folder...");
+            }
+
+            public int[] GetRandomNumbers(int maxCount) {
+                return new int[] {1,2,3,4};
+            }
+        }
+    "#;
+
+    let tree = parse(code);
+    let root_node = tree.root_node();
+    let mut methods = Vec::new();
+
+    for node in root_node.children(&mut root_node.walk()) {
+        if node.kind() == "class_declaration" {
+            if let Some(class_name_node) = node.child_by_field_name("name") {
+                let class_name = code[class_name_node.byte_range()].to_string();
+
+                if let Some(body) = node.child_by_field_name("body") {
+                    for member in body.children(&mut body.walk()) {
+                        if member.kind() == "method_declaration" {
+                            let method_name = member
+                                .child_by_field_name("name")
+                                .map(|n| code[n.byte_range()].to_string())
+                                .unwrap_or("Unknown".to_string());
+
+                            methods.push((class_name.clone(), method_name));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    assert_eq!(
+        methods,
+        vec![
+            ("Einstein".to_string(), "Add".to_string(),),
+            ("Einstein".to_string(), "Mul".to_string(),),
+            ("Einstein".to_string(), "ClearTemp".to_string(),),
+            ("Einstein".to_string(), "GetRandomNumbers".to_string(),)
+        ]
+    );
 }
