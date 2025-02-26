dd51d60e92612336aca9d40eeb99ff1e7b4811a9
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2025-02-26 12:56:53 +0000

    Initiated a new project

---
+++ src/InterfaceExtractor/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace InterfaceExtractor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (!ValidateArguments(args, out string sourceFolder, out string outputFolder))
+            return;
+
+        Specialist.ProcessFiles(sourceFolder, outputFolder);
+    }
+
+    static bool ValidateArguments(string[] args, out string sourceFolder, out string outputFolder)
+    {
+        sourceFolder = outputFolder = string.Empty;
+
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Argument error");
+            return false;
+        }
+
+        sourceFolder = args[0];
+        outputFolder = args[1];
+
+        if (!Directory.Exists(sourceFolder) || !Directory.Exists(outputFolder))
+        {
+            Console.WriteLine("Directory error");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/InterfaceExtractor/Specialist.cs
@@ -0,0 +1,8 @@
+namespace InterfaceExtractor;
+
+public static class Specialist {
+    public static void ProcessFiles(string sourceFolder, string outputFolder)
+    {
+        throw new NotImplementedException();
+    }
+}
