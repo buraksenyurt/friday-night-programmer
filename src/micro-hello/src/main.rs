a27ad3f26aaf418f945fe21907afe66188a9e737
buraksenyurt <burakselimsenyurt@gmail.com>
2025-03-22 21:06:13 +0300

    Changed code to blink rust

--- src/micro-hello/src/main.rs
+++ src/micro-hello/src/main.rs
@@ -9,7 +9,7 @@ use panic_halt as _;
 
 #[entry]
 fn start() -> ! {
-    let mut board = Board::take().unwrap();
+    let board = Board::take().unwrap();
     // Tüm Led'ler kapalı
     let clear = [
         [0, 0, 0, 0, 0],
@@ -19,23 +19,52 @@ fn start() -> ! {
         [0, 0, 0, 0, 0],
     ];
 
-    // Belli ledler açık ve H harfi şeklinde yanıyor.
-    let H = [
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
+    // Belli ledler açık ve R harfi şeklinde yanıyor.
+    let r = [
+        [1, 1, 1, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 1, 1, 0, 0],
+        [1, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0],
     ];
+
+    let u = [
+        [1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0],
+        [1, 1, 1, 1, 0],
+    ];
+
+    let s = [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0],
+        [1, 1, 1, 1, 0],
+    ];
+
+    let t = [
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+    ];
+
     // Matrisleri kullanabilecek bir Display nesnesi
     let mut display = Display::new(board.display_pins);
     let mut timer = Timer::new(board.TIMER0);
-
+    const WAIT:u32  = 500;
     loop {
-        // 1 saniye boyunca clear matrisine göre led'ler konumlanacak (Hepsi Kapalı)
-        display.show(&mut timer, clear, 1000);
-        // 1 saniye boyunca H matrisindeki Led'ler yanacak
-        display.show(&mut timer, H, 1000);
+        // 250 milisaniye boyunca clear matrisine göre led'ler konumlanacak (Hepsi Kapalı)
+        display.show(&mut timer, clear, WAIT);
+        // 250 saniye boyunca R matrisindeki Led'ler yanacak
+        display.show(&mut timer, r, WAIT);
+        display.show(&mut timer, u, WAIT);
+        display.show(&mut timer, s, WAIT);
+        display.show(&mut timer, t, WAIT);
+
         // ve bu böyle sürüp gidecek (Blinky effect)
     }
 }
