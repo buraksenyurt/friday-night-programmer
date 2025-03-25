3e5c6af901d7d0ab5825f54a2edb0696e2e23be9
buraksenyurt <burakselimsenyurt@gmail.com>
2025-03-25 13:11:26 +0300

    Refactored codes

--- src/micro-hello/src/main.rs
+++ src/micro-hello/src/main.rs
@@ -2,6 +2,9 @@
 #![no_main]
 #![no_std]
 
+mod letter_pipe;
+
+use crate::letter_pipe::{Letter, get_letter};
 use cortex_m_rt::entry;
 use microbit::display::blocking::Display;
 use microbit::{board::Board, hal::timer::Timer};
@@ -10,60 +13,19 @@ use panic_halt as _;
 #[entry]
 fn start() -> ! {
     let board = Board::take().unwrap();
-    // Tüm Led'ler kapalı
-    let clear = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-    ];
-
-    // Belli ledler açık ve R harfi şeklinde yanıyor.
-    let r = [
-        [1, 1, 1, 0, 0],
-        [1, 0, 0, 1, 0],
-        [1, 1, 1, 0, 0],
-        [1, 0, 1, 0, 0],
-        [1, 0, 0, 1, 0],
-    ];
-
-    let u = [
-        [1, 0, 0, 1, 0],
-        [1, 0, 0, 1, 0],
-        [1, 0, 0, 1, 0],
-        [1, 0, 0, 1, 0],
-        [1, 1, 1, 1, 0],
-    ];
-
-    let s = [
-        [1, 1, 1, 1, 0],
-        [1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 0],
-        [0, 0, 0, 1, 0],
-        [1, 1, 1, 1, 0],
-    ];
-
-    let t = [
-        [1, 1, 1, 1, 1],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-    ];
 
     // Matrisleri kullanabilecek bir Display nesnesi
     let mut display = Display::new(board.display_pins);
     let mut timer = Timer::new(board.TIMER0);
-    const WAIT:u32  = 500;
+    const WAIT: u32 = 500;
     loop {
         // 250 milisaniye boyunca clear matrisine göre led'ler konumlanacak (Hepsi Kapalı)
-        display.show(&mut timer, clear, WAIT);
+        display.show(&mut timer, get_letter(Letter::Clear), WAIT);
         // 250 saniye boyunca R matrisindeki Led'ler yanacak
-        display.show(&mut timer, r, WAIT);
-        display.show(&mut timer, u, WAIT);
-        display.show(&mut timer, s, WAIT);
-        display.show(&mut timer, t, WAIT);
+        display.show(&mut timer, get_letter(Letter::R), WAIT);
+        display.show(&mut timer, get_letter(Letter::U), WAIT);
+        display.show(&mut timer, get_letter(Letter::S), WAIT);
+        display.show(&mut timer, get_letter(Letter::T), WAIT);
 
         // ve bu böyle sürüp gidecek (Blinky effect)
     }
