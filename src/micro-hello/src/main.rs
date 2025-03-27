ec79a8a390cefafd5ac8e7df95359b51b7b7e68a
buraksenyurt <burakselimsenyurt@gmail.com>
2025-03-27 17:40:54 +0300

    Refactored with button press

--- src/micro-hello/src/main.rs
+++ src/micro-hello/src/main.rs
@@ -8,25 +8,28 @@ use crate::letter_pipe::{Letter, get_letter};
 use cortex_m_rt::entry;
 use microbit::display::blocking::Display;
 use microbit::{board::Board, hal::timer::Timer};
+use embedded_hal::digital::InputPin;
 use panic_halt as _;
 
 #[entry]
 fn start() -> ! {
-    let board = Board::take().unwrap();
+    let mut board = Board::take().unwrap();
 
     // Matrisleri kullanabilecek bir Display nesnesi
     let mut display = Display::new(board.display_pins);
     let mut timer = Timer::new(board.TIMER0);
     const WAIT: u32 = 500;
     loop {
-        // 250 milisaniye boyunca clear matrisine göre led'ler konumlanacak (Hepsi Kapalı)
-        display.show(&mut timer, get_letter(Letter::Clear), WAIT);
-        // 250 saniye boyunca R matrisindeki Led'ler yanacak
-        display.show(&mut timer, get_letter(Letter::R), WAIT);
-        display.show(&mut timer, get_letter(Letter::U), WAIT);
-        display.show(&mut timer, get_letter(Letter::S), WAIT);
-        display.show(&mut timer, get_letter(Letter::T), WAIT);
-
+        // A düğmesine bastığımız sürece RUST yazısını çalıştırıyoruz.
+        if let Ok(true) = board.buttons.button_a.is_low() {
+            // 250 milisaniye boyunca clear matrisine göre led'ler konumlanacak (Hepsi Kapalı)
+            display.show(&mut timer, get_letter(Letter::Clear), WAIT);
+            // 250 saniye boyunca R matrisindeki Led'ler yanacak
+            display.show(&mut timer, get_letter(Letter::R), WAIT);
+            display.show(&mut timer, get_letter(Letter::U), WAIT);
+            display.show(&mut timer, get_letter(Letter::S), WAIT);
+            display.show(&mut timer, get_letter(Letter::T), WAIT);
+        }
         // ve bu böyle sürüp gidecek (Blinky effect)
     }
 }
