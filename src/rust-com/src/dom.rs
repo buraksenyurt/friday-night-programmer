5b851adb2111a2c169de25d1b44336b6fe06d463
buraksenyurt <burakselimsenyurt@gmail.com>
2025-05-02 14:27:42 +0300

    Applied ordering

--- src/rust-com/src/dom.rs
+++ src/rust-com/src/dom.rs
@@ -17,25 +17,18 @@ pub fn render(node: &Node) -> String {
     match node {
         Node::Text(text) => text.clone(),
         Node::Element(e) => {
-            let attrs = e
-                .attributes
-                .iter()
-                .map(|(k, v)| format!("{}=\"{}\"", k, v))
-                .collect::<Vec<String>>()
-                .join(" ");
-            let children = e
-                .children
-                .iter()
-                .map(|child| render(child))
-                .collect::<Vec<String>>()
-                .join("");
+            let mut attrs_vec: Vec<_> = e.attributes.iter().collect();
+            attrs_vec.sort_by_key(|(k, _)| *k);
 
-            format!(
-                "<{tag} {attrs}>{children}</{tag}>",
-                tag = e.tag,
-                attrs = attrs,
-                children = children
-            )
+            let attrs = attrs_vec
+                .into_iter()
+                .map(|(k, v)| format!(r#"{k}="{v}""#))
+                .collect::<Vec<_>>()
+                .join(" ");
+
+            let children = e.children.iter().map(render).collect::<Vec<_>>().join("");
+
+            format!("<{tag} {attrs}>{children}</{tag}>", tag = e.tag)
         }
     }
 }
