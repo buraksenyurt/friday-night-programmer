da9c31ac5fb08af371d1ea9471d78aa8b73327bf
buraksenyurt <burakselimsenyurt@gmail.com>
2025-05-02 13:47:37 +0300

    Setup logger

--- src/rust-com/src/main.rs
+++ src/rust-com/src/main.rs
@@ -1,14 +1,14 @@
 use axum::Router;
 use axum::response::Html;
 use axum::routing::get;
+use log::{info, warn};
 use std::collections::HashMap;
-use log::info;
 use tokio::io;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    env_logger::init();
-    
+    setup_log();
+
     let app = Router::new().route("/", get(index_handler));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:1903").await?;
     info!("Server is listening on: 0.0.0.0:1903");
@@ -17,7 +17,17 @@ async fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn setup_log() {
+    dotenvy::dotenv().ok();
+    let log_level = std::env::var("LOG_LEVEL").unwrap_or_else(|_| "info".into());
+    unsafe {
+        std::env::set_var("RUST_LOG", &log_level);
+    }
+    env_logger::init();
+}
+
 async fn index_handler() -> Html<String> {
+    warn!("Index Handler call");
     let button = Button {
         label: "Save".to_string(),
         class: "btn btn-primary".to_string(),
