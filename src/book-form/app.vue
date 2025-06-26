218bcf71078bf5e4feb77d1b26be26f16241783c
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-26 18:51:29 +0300

    Initiated a nuxt hello world

---
+++ src/book-form/app.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const title = 'Rent a Book Form'
+const description = 'A form to rent a book from the library'
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <p>{{ description }}</p>
+</template>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+  font-size: 2em;
+  margin-bottom: 0.5em;
+}
+
+p {
+  color: #34495e;
+  font-size: 1.2em;
+}
+</style>
