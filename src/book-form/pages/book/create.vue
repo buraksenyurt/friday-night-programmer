ee8352255e1b124c0bb836b3a8138c1a3d4197fc
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-26 19:40:24 +0300

    Added a in-memory based post

--- src/book-form/pages/book/create.vue
+++ src/book-form/pages/book/create.vue
@@ -1,12 +1,62 @@
 <script setup lang="ts">
 const title = 'Rent a Book Form'
-const description = 'A form to rent a book from the library'
+const success = ref(false)
+
+const book = reactive({
+  title: '',
+  author: '',
+  published: '',
+  hugoYear: ''
+})
+
+const submit = async () => {
+  const res = await $fetch('/api/books', {
+    method: 'POST',
+    body: book
+  })
+
+  if (res.success) {
+    success.value = true
+  }
+}
+
 </script>
 
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ description }}</p>
+
+    <form @submit.prevent="submit">
+      <table>
+        <tr>
+          <td>
+            <input v-model="book.title" placeholder="Book title" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input v-model="book.author" placeholder="Author" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input v-model="book.published" placeholder="Published" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input v-model="book.hugoYear" placeholder="Hugo Year" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button type="submit">Add</button>
+          </td>
+        </tr>
+      </table>
+    </form>
+
+    <p v-if="success">Success</p>
 
     <p>
       <NuxtLink to="/">
--- src/book-form/server/api/books.ts
+++ src/book-form/server/api/books.ts
@@ -1,34 +1,45 @@
-export default defineEventHandler(() => {
-    return [
-        {
-            title: "The Left Hand of Darkness",
-            author: "Ursula K. Le Guin",
-            published: 1969,
-            hugoYear: 1970,
-        },
-        {
-            title: "Neuromancer",
-            author: "William Gibson",
-            published: 1984,
-            hugoYear: 1985,
-        },
-        {
-            title: "Hyperion",
-            author: "Dan Simmons",
-            published: 1989,
-            hugoYear: 1990,
-        },
-        {
-            title: "A Deepness in the Sky",
-            author: "Vernor Vinge",
-            published: 1999,
-            hugoYear: 2000,
-        },
-        {
-            title: "The Three-Body Problem",
-            author: "Liu Cixin",
-            published: 2006,
-            hugoYear: 2015,
+let books = [
+    {
+        title: "The Left Hand of Darkness",
+        author: "Ursula K. Le Guin",
+        published: 1969,
+        hugoYear: 1970,
+    },
+    {
+        title: "Neuromancer",
+        author: "William Gibson",
+        published: 1984,
+        hugoYear: 1985,
+    },
+    {
+        title: "Hyperion",
+        author: "Dan Simmons",
+        published: 1989,
+        hugoYear: 1990,
+    },
+    {
+        title: "A Deepness in the Sky",
+        author: "Vernor Vinge",
+        published: 1999,
+        hugoYear: 2000,
+    },
+    {
+        title: "The Three-Body Problem",
+        author: "Liu Cixin",
+        published: 2006,
+        hugoYear: 2015,
+    }
+];
+export default defineEventHandler(async (event) => {
+    if (event.method === 'GET') {
+        return books
+    }
+    if (event.method === 'POST') {
+        const body = await readBody(event)
+        books.push(body)
+        return {
+            success: true,
+            message: "Book added successfully"
         }
-    ]
+    }
 })
