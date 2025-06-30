2536cbab8187a2c648b8c44b7093854b96307956
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-30 11:37:15 +0300

    Added bootstrap based alert

--- src/book-form/pages/book/create.vue
+++ src/book-form/pages/book/create.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 const title = 'Rent a Book Form'
 
-const success = ref(false)
+const message = ref('')
+const messageType = ref<'success' | 'error' | 'warning' | 'info'>('info')
+
+const showAlert = (type: typeof messageType.value, text: string) => {
+  messageType.value = type,
+    message.value = text
+}
+const clearAlert = () => {
+  message.value = ''
+}
 
 const addBook = async (book: Book) => {
   const res = await postBook(book)
   if (res.success) {
-    success.value = true
+    showAlert('success', res.message)
+  } else {
+    showAlert('error', res.message)
   }
 }
 
@@ -18,12 +29,13 @@ const addBook = async (book: Book) => {
 
     <BookForm @submit="addBook" />
 
-    <p v-if="success">Success</p>
-
     <p>
       <NuxtLink to="/">
         Go to main page
       </NuxtLink>
     </p>
+
+    <AlertBox v-if="message" :type="messageType" :message="message" @close="clearAlert" />
+
   </div>
 </template>
