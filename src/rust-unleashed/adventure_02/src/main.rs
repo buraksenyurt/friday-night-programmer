d8a7beae650844a25d73f7a9673151057a09ec53
buraksenyurt <burakselimsenyurt@gmail.com>
2025-11-08 20:45:38 +0300

    Implement game loop structure with state transitions and main function logic

--- src/rust-unleashed/adventure_02/src/main.rs
+++ src/rust-unleashed/adventure_02/src/main.rs
@@ -1,3 +1,8 @@
+#![allow(dead_code)]
+
+use std::marker::PhantomData;
+use std::mem;
+
 struct MenuState;
 struct PlayingState;
 struct PausedState;
@@ -11,11 +16,65 @@ impl Default for GameLoop<MenuState> {
     fn default() -> Self {
         GameLoop { state: PhantomData }
     }
+}
 
-    fn start(self) -> GameLoop<PlayingState> {
-        println!("Starting the game...");
+impl<State> GameLoop<State> {
+    fn change<NextState>(&self) -> GameLoop<NextState> {
         GameLoop { state: PhantomData }
+    }
+    fn reset(&self) -> Self {
+        Self { state: PhantomData }
+    }
+    fn get_state(&self) -> PhantomData<State> {
+        self.state
     }
 }
 
-fn main() {}
+impl GameLoop<MenuState> {
+    fn play(&self) -> GameLoop<PlayingState> {
+        println!("Playing...");
+        self.change::<PlayingState>()
+    }
+}
+
+impl GameLoop<PlayingState> {
+    fn pause(&self) -> GameLoop<PausedState> {
+        println!("Game paused...");
+        self.change::<PausedState>()
+    }
+    fn loose(&self) -> GameLoop<GameOverState> {
+        println!("Game over...");
+        self.change::<GameOverState>()
+    }
+}
+
+impl GameLoop<GameOverState> {
+    fn go_to_menu(&self) -> Self {
+        println!("Going to menu");
+        self.reset()
+    }
+}
+
+impl GameLoop<PausedState> {
+    fn go_on(&self) -> GameLoop<PlayingState> {
+        println!("Playing...");
+        self.change::<PlayingState>()
+    }
+}
+
+fn main() {
+    let flow = GameLoop::default().play();
+
+    println!(
+        "Size of Gameloop is {} bytes and the current state is {:?}",
+        mem::size_of_val(&flow),
+        flow.get_state()
+    );
+
+    let flow = flow.pause().go_on().loose().go_to_menu();
+    println!(
+        "Size of Gameloop is {} bytes and the current state is {:?}",
+        mem::size_of_val(&flow),
+        flow.get_state()
+    );
+}
