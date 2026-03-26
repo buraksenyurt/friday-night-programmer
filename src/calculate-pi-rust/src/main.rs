e8f0779b1f76d1437f9a7ae26dee1d13c60b1d6b
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-03-26 16:25:43 +0300

    Refactor π calculation to use calculate_pi_v1 and add calculate_pi_v0 for comparison

--- src/calculate-pi-rust/src/main.rs
+++ src/calculate-pi-rust/src/main.rs
@@ -7,13 +7,14 @@ fn main() {
 
     for _ in 0..10 {
         let start_time = std::time::Instant::now();
-        let pi_estimate = calculate_pi(total_iterations);
+        //let pi_estimate = calculate_pi_v0(total_iterations);
+        let pi_estimate = calculate_pi_v1(total_iterations);
         let elapsed = start_time.elapsed();
         println!("Estimated Pi: {} in {:?}", pi_estimate, elapsed);
     }
 }
 
-fn calculate_pi(total_iterations: u64) -> f64 {
+fn calculate_pi_v1(total_iterations: u64) -> f64 {
     let chunk_size = 4; // SIMD için 4'lü gruplar halinde işlem yapacağız
                         // çünkü AVX2 256-bit genişliğinde ve 4 adet f64 (64-bit) değeri aynı anda işleyebilir.
     let loop_count = total_iterations / chunk_size;
@@ -49,4 +50,24 @@ pub fn get_circle_value(x_values: &[f64; 4], y_values: &[f64; 4]) -> u64 {
     }
 
     count
+}
+
+fn calculate_pi_v0(total_iterations: u64) -> f64 {
+    let in_circle: u64 = (0..total_iterations)
+        .into_par_iter()
+        .map_init(
+            || rand::make_rng::<SmallRng>(),
+            |rng, _| {
+                let (x, y): (f64, f64) = rng.random();
+
+                if x * x + y * y <= 1.0 {
+                    1_u64
+                } else {
+                    0_u64
+                }
+            },
+        )
+        .sum();
+
+    4.0 * (in_circle as f64) / (total_iterations as f64)
 }
