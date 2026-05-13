baf6f1fb96c7ccfb3343aa5ff06d47aa1adb0a98
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-05-13 22:16:23 +0300

    Add another function-like macro to understand simple usage of TokenStream

--- src/rust-proc-macros/app/src/main.rs
+++ src/rust-proc-macros/app/src/main.rs
@@ -1,4 +1,6 @@
-use framework_macros::greetings;
+use framework_macros::{greetings, invoice_code};
 fn main() {
-    greetings!("Input...");
+    greetings!("Input..."); // function-like macro
+    let invoice = invoice_code!("VEH","BG",1001);
+    println!("Created invoice number: {}", invoice);
 }
--- src/rust-proc-macros/framework-macros/src/lib.rs
+++ src/rust-proc-macros/framework-macros/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
 
+// function-like macro
 #[proc_macro]
 pub fn greetings(input: TokenStream) -> TokenStream {
     dbg!(input);
@@ -9,6 +10,50 @@ pub fn greetings(input: TokenStream) -> TokenStream {
         println!("Greetings from proc macro");
      }
      "#
-    .parse()
-    .unwrap()
+        .parse()
+        .unwrap()
 }
+
+/*
+    Daha işe yarar bir function-like macro
+    Şöyle kullanabiliriz örneğin;
+
+    invoice_code!("VEH","BG",1001);
+
+    Tabii gerçek hayat senaryolarında tercih etmeyiz zira parse işlemleri hataya çok müsaittir.
+    Bu şekilde string parse yerine syn, quote gibi endüstri standardı haline gelmiş token bazlı çalışan
+    crate'ler kullanmamız gerekir. Bu ve önceki örnek TokenStream'in doğasını anlamak için
+    yeterlidir.
+*/
+
+#[proc_macro]
+pub fn invoice_code(input: TokenStream) -> TokenStream {
+    let raw = input.to_string(); // gelen token stream'i alalım
+    // sonra virgüle göre parçalarını alalım
+    let parts = raw.split(',').map(|p| p.trim()).collect::<Vec<&str>>();
+
+    // bir kontrol yapalım
+    if parts.len() != 3 {
+        return r#"compile_error!("wrong number of arguments!")"#.parse().unwrap();
+    }
+
+    let module_code = parts[0];
+    let invoice_type = parts[1];
+    /*
+        Macrolar derleme zamanı için kod üretirler ama çalışma zamanında da harici kütüphaneleri
+        ele alabilirler. Mesela üretilen fatura numarasına chrono crate üzerinden güncel
+        tarihe ait yil, ay, gün bilgisini ekleyebiliriz.
+     */
+    let build_date = chrono::Local::now().format("%Y%m%d").to_string();
+    let id = parts[2];
+
+
+    let output = format!(
+        r#"
+            format!("INV-{{}}-{{}}-{{}}-{{}}",{module_code},{invoice_type},{build_date},{id})
+        "#
+    );
+
+    output.parse().unwrap()
+}
+
