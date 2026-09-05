2cc92f74cb533abfbe9b6074ae9160a01dd75aa1
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-09-05 14:10:27 +0300

    Descease reflection cost in Apply function calls for Scheduler

--- src/BevyDotNet/BevyDotNet.Library/Scheduler.cs
+++ src/BevyDotNet/BevyDotNet.Library/Scheduler.cs
@@ -1,4 +1,6 @@
-﻿namespace BevyDotNet.Library;
+﻿using System.Reflection;
+
+namespace BevyDotNet.Library;
 
 public enum SystemState
 {
@@ -14,6 +16,42 @@ public partial class Scheduler(World world)
         { SystemState.Update, new List<object>() }
     };
 
+    /*
+     * Run metodundaki döngüde her seferinde girdiğimiz bir reflection maliyeti var.
+     * Sistem tipini buluyoruz, kullandığı Component tiplerinden yola çıkarak Query sınıfını bulup
+     * bir örneğini oluşturuyor, GetEntities ve Apply metodlarını çağırıyoruz. Bunun maliyeti şu anda oldukça yüksek.
+     * Her döngü her sistem için reflection yapıyor. Bunu önlemek için bir cache mekanizması kullanılabilir.
+     * 
+     * SystemInvoker adında bir record oluşturuyoruz. 
+     * Sistemin tipini, kullandığı Query tipini, GetEntities ve Apply metodlarını tutuyor.
+     * Her tür için eşleşen bir SystemInvoker record nesnesi söz konusu. Bu sadece okunabilir ve değiştirilemez bir cache olarak saklanıyor.
+     * 
+     * Run metodundaki for döngüsü her seferinde önce GetOrCreateInvoker metodunu çağırıyor. 
+     * Bu metod eğer daha önce bu sistem tipi için bir invoker oluşturulmuşsa üretiyor eğer varsa da onu kullanıyor.
+     * 
+     */
+    private sealed record SystemInvoker(Type QueryType, MethodInfo GetEntitiesMethod, MethodInfo ApplyMethod);
+    private static readonly Dictionary<Type, SystemInvoker> _systemInvokerCache = [];
+    private static SystemInvoker GetInvoker(Type instanceType)
+    {
+        if (_systemInvokerCache.TryGetValue(instanceType, out var invoker))
+        {
+            return invoker;
+        }
+        var systemType = instanceType
+            .GetInterfaces()
+            .First(i => i.IsGenericType && GeneratedSystemInterfaces.Contains(i.GetGenericTypeDefinition()));
+
+        var componentTypes = systemType.GetGenericArguments();
+        var queryType = GeneratedQueryTypes[componentTypes.Length - 1].MakeGenericType(componentTypes);
+        var getEntitiesMethod = queryType.GetMethod("GetEntities")!;
+        var applyMethod = systemType.GetMethod("Apply")!;
+        invoker = new SystemInvoker(queryType, getEntitiesMethod, applyMethod);
+
+        _systemInvokerCache[instanceType] = invoker;
+        return invoker;
+    }
+
     public void Run(SystemState state)
     {
         if (!_systems.TryGetValue(state, out List<object>? value)) return;
@@ -22,20 +60,12 @@ public partial class Scheduler(World world)
 
         foreach (var system in value)
         {
-            var systemType = system
-                .GetType()
-                .GetInterfaces()
-                .FirstOrDefault(i => i.IsGenericType && GeneratedSystemInterfaces.Contains(i.GetGenericTypeDefinition()));
+            var invoker = GetInvoker(system.GetType());
 
-            if (systemType == null) continue;
+            var queryInstance = Activator.CreateInstance(invoker.QueryType, world)!;
+            var entities = invoker.GetEntitiesMethod.Invoke(queryInstance, null)!;
 
-            var componentTypes = systemType.GetGenericArguments();
-            var queryType = GeneratedQueryTypes[componentTypes.Length - 1].MakeGenericType(componentTypes);
-
-            var queryInstance = Activator.CreateInstance(queryType, world);
-            var entities = queryType.GetMethod("GetEntities")!.Invoke(queryInstance, null);
-
-            systemType.GetMethod("Apply")!.Invoke(system, [entities, commands]);
+            invoker.ApplyMethod.Invoke(system, [entities, commands]);
         }
 
         commands.Flush(world);
